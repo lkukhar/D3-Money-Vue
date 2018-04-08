8f52ec7e2d6359f062fc278b8d4bfff878dcb30d
lkukhar <16503599+lkukhar@users.noreply.github.com>
2018-04-07 22:06:07 -0400

    Add Budgeting Component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <div id="app">
-    <Finances/>
+  <div id="app" class="container">
+    <h1 class="text-center">D3 Money Vue</h1>
+    <h2 class="text-center">Visualize Your Finances</h2>
+    <Budgeting />
   </div>
 </template>
 
+<!-- D3 CDN Link -->
+<script src="https://d3js.org/d3.v5.min.js"></script>
 <script>
-import Investing from './components/Investing'
+  import Budgeting from './components/Budgeting'
+  //import Investing from './components/Investing'
 
-export default {
-  name: 'App',
-  components: {
-    Investing
+  export default {
+    name: 'App',
+    components: {
+      //Investing
+      Budgeting
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Budgeting.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="budget">
+    <h3 class="text-center">Budgeting</h3>
+    <h4 class="text-center">See where your money goes</h4>
+    <small>Enter the amount you spend per month in the following fields</small>
+    <!-- INPUT -->
+    <div class="col-md-4">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+    </div>
+  <!-- GRAPH -->
+    <div>
+
+    </div>
+
+  </div>
+</template>
+<script>
+
+</script>
+
+<style scoped>
+  #budget{
+    background-color: grey;
+  }
+<style>
