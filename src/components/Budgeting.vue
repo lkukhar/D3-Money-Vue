402bdb317869a92ad7e74e1bba74c91137971b7b
lkukhar <16503599+lkukhar@users.noreply.github.com>
2018-04-29 01:46:49 -0400

    Add More Styling, Remoove Duplicate Code In Mounted Section, Replace With Method, Insert Conditional Into Method

--- src/components/Budgeting.vue
+++ src/components/Budgeting.vue
@@ -7,23 +7,22 @@
           <!-- SUBHEADER -->
           <div class="row">
             <div class="col-md-12">
-              <h3 class="text-center">Budgeting</h3>
-              <h4 class="text-center">See where your money goes!</h4>
+              <h3 class="text-center">Budget</h3>
+              <h4 class="text-center">Visualize your spending!</h4>
             </div>
           </div>
           <!-- END SUB HEADER -->
 
           <!-- INPUT AND CHART -->
-          <div class="row">
-
+          <div class="row sub-budget">
             <!-- INPUT -->
             <div class="col-md-6">
-              <span>Enter the amount you spend <em><b>per month</b></em> for:</span>
-              <div v-for="item in moneyItems" class="money-item-fields">
+              <span id="instruction">Enter the amount you spend <em><b>per month</b></em> for:</span>
+              <div v-for="item in expenseItems" class="money-item-fields">
                 <div>
                   <b>{{item.label}}:</b>
                   <div class="money-item-amount float-right">
-                    $<input type="number" step=".01" min="0" v-on:input="updateChart" v-model.number="item.count" v-bind:placeholder="item.label"/>
+                    $<input type="number" step=".01" min="0" v-on:input="createChart(true)" v-model.number="item.count" v-bind:placeholder="item.label"/>
                   </div>
                 </div>
               </div>
@@ -52,86 +51,24 @@
     name: 'Budgeting',
     data () {
       return {
-        moneyItems: [
-          {label: 'Credit Card',     count: 1},
-          {label: 'Utilities',       count: 0},
-          {label: 'Rent',            count: 0},
-          {label: 'Car & Insurance', count: 0},
+        expenseItems: [
+          {label: 'Utilities',       count: 1},
+          {label: 'Rent/Mortgage',   count: 0},
+          {label: 'Car',             count: 0},
           {label: 'Groceries',       count: 0},
-          {label: 'Insurance',       count: 0},
-          {label: 'Savings',         count: 0},
           {label: 'Entertainment',   count: 0},
-          {label: 'Investments',     count: 0}
+          {label: 'Miscellaneous',   count: 0}
         ]
-
       }
     },
     mounted (){
-      const width = 360;
-      const height = 360;
-      const donutWidth = 50;
-      const legendRectSize = 18;
-      const legendSpacing = 4;
-      var radius = Math.min(width, height) / 2;
-
-      var color = d3.scaleOrdinal().range(['#A60F2B', '#648C85', '#B3F2C9',
-                                           '#528C18', '#C3F25C', '#FFB732',
-                                           '#128518', '#5F6318', '#B266B2']);
-
-      var svg = d3.select('#budget-chart')
-                  .append('svg')
-                  .attr('width', width)
-                  .attr('height', height)
-                  .append('g')
-                  .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
-      var arc = d3.arc()
-                  .innerRadius(radius - donutWidth)
-                  .outerRadius(radius);
-      var pie = d3.pie()
-                  .value(function(d) { return d.count; })
-                  .sort(null);
-      var path = svg.selectAll('path')
-                    .data(pie(this.moneyItems))
-                    .enter()
-                    .append('path')
-                    .attr('d', arc)
-                    .attr('fill', function(d, i) {
-                       return color(d.data.label);
-                    });
-      var legend = svg.selectAll('.legend')
-                .data(color.domain())
-                .enter()
-                .append('g')
-                .attr('class', 'legend')
-                .attr('transform', function(d, i) {
-                  var height = legendRectSize + legendSpacing;
-                  var offset =  height * color.domain().length / 2;
-                  var horz = -2 * legendRectSize;
-                  var vert = i * height - offset;
-                  return 'translate(' + horz + ',' + vert + ')';
-                });
-      var tooltip = d3.select('#chart')
-                      .append('div')
-                      .attr('class', 'tooltip');
-      tooltip.append('div')
-             .attr('class', 'label');
-      tooltip.append('div')
-             .attr('class', 'count');
-      tooltip.append('div')
-             .attr('class', 'percent');
-      legend.append('rect')
-            .attr('width', legendRectSize)
-            .attr('height', legendRectSize)
-            .style('fill', color)
-            .style('stroke', color);
-      legend.append('text')
-            .attr('x', legendRectSize + legendSpacing)
-            .attr('y', legendRectSize - legendSpacing)
-            .text(function(d) { return d; });
+      this.createChart(false); //Create original chart, without removal of anything
     },
     methods: {
-      updateChart : function(){
-        d3.selectAll("svg").remove();
+      createChart : function(isUpdate){
+        if(isUpdate){
+          d3.selectAll("svg").remove(); //Remove old chart, then recreate if true
+        }
         const width = 360;
         const height = 360;
         const donutWidth = 50;
@@ -156,7 +93,7 @@
                     .value(function(d) { return d.count; })
                     .sort(null);
         var path = svg.selectAll('path')
-                      .data(pie(this.moneyItems))
+                      .data(pie(this.expenseItems))
                       .enter()
                       .append('path')
                       .attr('d', arc)
@@ -175,15 +112,6 @@
                           var vert = i * height - offset;
                           return 'translate(' + horz + ',' + vert + ')';
                         });
-        var tooltip = d3.select('#chart')
-                        .append('div')
-                        .attr('class', 'tooltip');
-        tooltip.append('div')
-               .attr('class', 'label');
-        tooltip.append('div')
-               .attr('class', 'count');
-        tooltip.append('div')
-               .attr('class', 'percent');
         legend.append('rect')
               .attr('width', legendRectSize)
               .attr('height', legendRectSize)
@@ -200,8 +128,20 @@
 </script>
 
 <style scoped>
+  span{
+    margin-left: 30px;
+  }
+
+  rect {
+    stroke-width: 2;
+  }
+  input{
+    width: 70%;
+    float:right;
+  }
+
   .money-item-fields{
-    padding:5px 0px 5px 0px;
+    padding:5px 0px 5px 30px;
   }
   .money-item-amount{
     display: inline-block;
@@ -211,30 +151,22 @@
   .legend {
     font-size: 12px;
   }
-  .tooltip {
-    background: #eee;
-    box-shadow: 0 0 5px #999999;
-    color: #333;
-    display: none;
-    font-size: 12px;
-    left: 130px;
-    padding: 10px;
-    position: absolute;
-    text-align: center;
-    top: 95px;
-    width: 80px;
-    z-index: 10;
+  .sub-budget{
+    margin: 0px 40px;
+    border-top: 1px solid lightgrey;
   }
-  rect {
-    stroke-width: 2;
-  }
-  input{
-    width: 70%;
-    float:right;
-  }
+
   #budget-chart{
     height: 360px;
     width: 360px;
     position: relative;
+  }
+  #instruction{
+    display: inline-block;
+    padding: 25px 0px ;
+  }
+  #budgeting{
+    background-color: whitesmoke;
+    border: 1px solid lightgrey;
   }
 </style>
