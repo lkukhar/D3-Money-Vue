e647aab7c175ea7bd7a666b429fef626bd1b5779
lkukhar <16503599+lkukhar@users.noreply.github.com>
2018-05-11 02:22:00 -0400

    Refactor Data, Add Remove Method, Add Color Picker, Prep HTML for Conversion To Dynamic Budget List

--- src/components/Budgeting.vue
+++ src/components/Budgeting.vue
@@ -17,12 +17,25 @@
           <div class="row sub-budget">
             <!-- INPUT -->
             <div class="col-md-6">
-              <span id="instruction">Enter the amount you spend <em><b>per month</b></em> for:</span>
-              <div v-for="item in expenseItems" class="money-item-fields">
+              <div>
+                <span class="instruction">Enter another budget item:</span>
                 <div>
-                  <b>{{item.label}}:</b>
-                  <div class="money-item-amount float-right">
-                    $<input type="number" step=".01" min="0" v-on:input="createChart(true)" v-model.number="item.count" v-bind:placeholder="item.label"/>
+                  <input type="color" id="color-picker" />
+                  <input name="new-budget-item" id="new-budget-item" class="float-left new-budget" placeholder="New Item"/>
+                  <input name="new-budget-amount" id="new-budget-amount" class="float-left new-budget" placeholder="$ Amount"/>
+                  <button>Add</button>
+                </div>
+              </div>
+              <div>
+                <span class="instruction">Enter the amount you spend <em><b>per month</b></em> for:</span>
+                <div v-for="item in expenseItems" class="money-item-fields">
+                  <div>
+                    <div class="color"></div>
+                    <b>{{item.label}}:</b>
+                    <div class="money-item-amount float-right">
+                      <button class="float-right" type="button" name="delete" v-on:click="removeBudgetItem(item)">X</button>
+                      $<input type="number" step="1" min="0" v-on:input="createChart(true)" v-model.number="item.count" v-bind:placeholder="item.label"/>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -52,12 +65,11 @@
     data () {
       return {
         expenseItems: [
-          {label: 'Utilities',       count: 1},
-          {label: 'Rent/Mortgage',   count: 0},
-          {label: 'Car',             count: 0},
-          {label: 'Groceries',       count: 0},
-          {label: 'Entertainment',   count: 0},
-          {label: 'Miscellaneous',   count: 0}
+          {label: 'Utilities',       count: 0, color: '#A60F2B'},
+          {label: 'Rent',            count: 0, color: '#648C85'},
+          {label: 'Car',             count: 0, color: '#B3F2C9'},
+          {label: 'Groceries',       count: 0, color: '#528C18'},
+          {label: 'Entertainment',   count: 0, color: '#C3F25C'}
         ]
       }
     },
@@ -65,6 +77,12 @@
       this.createChart(false); //Create original chart, without removal of anything
     },
     methods: {
+      removeBudgetItem: function(item){
+        var index = this.expenseItems.indexOf(item);
+        if (index > -1) {
+          this.expenseItems.splice(index, 1);
+        }
+      },
       createChart : function(isUpdate){
         if(isUpdate){
           d3.selectAll("svg").remove(); //Remove old chart, then recreate if true
@@ -77,8 +95,7 @@
         var radius = Math.min(width, height) / 2;
 
         var color = d3.scaleOrdinal().range(['#A60F2B', '#648C85', '#B3F2C9',
-                                             '#528C18', '#C3F25C', '#FFB732',
-                                             '#128518', '#5F6318', '#B266B2']);
+                                             '#528C18', '#C3F25C']);
 
         var svg = d3.select('#budget-chart')
                     .append('svg')
@@ -100,27 +117,7 @@
                       .attr('fill', function(d, i) {
                          return color(d.data.label);
                       });
-        var legend = svg.selectAll('.legend')
-                        .data(color.domain())
-                        .enter()
-                        .append('g')
-                        .attr('class', 'legend')
-                        .attr('transform', function(d, i) {
-                          var height = legendRectSize + legendSpacing;
-                          var offset =  height * color.domain().length / 2;
-                          var horz = -2 * legendRectSize;
-                          var vert = i * height - offset;
-                          return 'translate(' + horz + ',' + vert + ')';
-                        });
-        legend.append('rect')
-              .attr('width', legendRectSize)
-              .attr('height', legendRectSize)
-              .style('fill', color)
-              .style('stroke', color);
-        legend.append('text')
-              .attr('x', legendRectSize + legendSpacing)
-              .attr('y', legendRectSize - legendSpacing)
-              .text(function(d) { return d; });
+
       }
     }
   }
@@ -140,13 +137,17 @@
     float:right;
   }
 
+  .instruction{
+    display: inline-block;
+    padding: 25px 0px ;
+  }
   .money-item-fields{
     padding:5px 0px 5px 30px;
   }
   .money-item-amount{
     display: inline-block;
     text-align: right;
-    margin-right: 100px;
+    margin-right:40px;
   }
   .legend {
     font-size: 12px;
@@ -155,18 +156,36 @@
     margin: 0px 40px;
     border-top: 1px solid lightgrey;
   }
-
+  .new-budget{
+    width:30%;
+  }
+  .color{
+    display:inline-block;
+    width:15px;
+    height:15px;
+    background-color: black;
+    margin-left:5px;
+  }
   #budget-chart{
     height: 360px;
     width: 360px;
     position: relative;
   }
-  #instruction{
-    display: inline-block;
-    padding: 25px 0px ;
-  }
   #budgeting{
     background-color: whitesmoke;
     border: 1px solid lightgrey;
+  }
+  #new-budget-item{
+
+    width:40%
+  }
+  #new-budget-amount{
+    width:32%
+  }
+  #color-picker{
+    width:5%;
+    float:left;
+    height:30px;
+    margin-left:6%;
   }
 </style>
